f1ffa44f6d80c9f31d1050870576df1e25790eb9
zenith <reeingspam@gmail.com>
2022-11-09 08:54:28 -0700

    adoption

--- js/binary.js
+++ js/binary.js
@@ -12,9 +12,39 @@ function input() {
     r = k % 2;
     k -= r;
     k = k / 2;
-    console.log(k);
     binaryOut.push(r);
   }
-  console.log(binaryOut);
-  document.getElementById("output").innerHTML = binaryOut;
+  binaryOut.push("0000");
+  output = binaryOut.join("");
+  let chunckedOut = [];
+  i = 0;
+  r = 0;
+  while (r < output.length) {
+    if ((i = 3)) {
+      chunckedOut.push(output.slice(r - 4, r));
+      chunckedOut.push(" ");
+      console.warn(output.slice(r - 4, r));
+    }
+    r += 4;
+    i++;
+  }
+  console.log(chunckedOut);
+  let final = [];
+  final = chunckedOut.join("").split("");
+  let reversed = final.reverse();
+  let finalOutput = reversed.join("");
+  document.getElementById("output").innerHTML = finalOutput;
+}
+
+function input2() {
+  let input = document.getElementById("input2").value;
+  let inputA = input.split("");
+  inputA = inputA.reverse();
+  let output = 0;
+  i = 0;
+  while (i < input.length) {
+    output += (2 ^ i) * inputA[i];
+    console.log(output);
+    i++;
+  }
 }
